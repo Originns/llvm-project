bfb3243d9e85f53c2536945ae3ec42e7d0229f41
Originns <68753984+Originns@users.noreply.github.com>
2025-05-20 13:53:05 +0200

    [flang-tidy][openmp] add openmp accumulator race check

---
+++ flang/tools/flang-tidy/openmp/AccumulatorRaceCheck.h
@@ -0,0 +1,34 @@
+#ifndef LLVM_FLANG_TOOLS_FLANG_TIDY_OPENMP_ACCUMULATORRACECHECK_H
+#define LLVM_FLANG_TOOLS_FLANG_TIDY_OPENMP_ACCUMULATORRACECHECK_H
+#include "../FlangTidyCheck.h"
+#include "flang/Parser/parse-tree.h"
+
+namespace Fortran::tidy::openmp {
+
+/// This check tries to detect potential race conditions in OpenMP accumulators.
+///
+/// For the user-facing documentation see:
+/// https://flang.llvm.org/@PLACEHOLDER@/accumulator-race.html
+class AccumulatorRaceCheck : public virtual FlangTidyCheck {
+public:
+  using FlangTidyCheck::FlangTidyCheck;
+  virtual ~AccumulatorRaceCheck() = default;
+
+  void Enter(const parser::OpenMPBlockConstruct &) override;
+  void Leave(const parser::OpenMPBlockConstruct &) override;
+  void Enter(const parser::OpenMPLoopConstruct &) override;
+  void Leave(const parser::OpenMPLoopConstruct &) override;
+  void Enter(const parser::OmpAtomicUpdate &) override;
+  void Leave(const parser::OmpAtomicUpdate &) override;
+  void Enter(const parser::OpenMPCriticalConstruct &) override;
+  void Leave(const parser::OpenMPCriticalConstruct &) override;
+  void Enter(const parser::AssignmentStmt &) override;
+
+private:
+  int inParallelRegion_ = 0;
+  int inCriticalSection_ = 0;
+  int inAtomicUpdate_ = 0;
+};
+
+} // namespace Fortran::tidy::openmp
+#endif // LLVM_FLANG_TOOLS_FLANG_TIDY_OPENMP_ACCUMULATORRACECHECK_H
