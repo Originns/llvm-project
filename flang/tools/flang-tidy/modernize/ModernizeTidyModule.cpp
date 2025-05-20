f63b3ed470f086e7bc352c99049d4d63d248533f
Originns <68753984+Originns@users.noreply.github.com>
2025-05-20 11:39:27 +0200

    [flang-tidy] add modernize module

---
+++ flang/tools/flang-tidy/modernize/ModernizeTidyModule.cpp
@@ -0,0 +1,24 @@
+#include "../FlangTidyModule.h"
+#include "../FlangTidyModuleRegistry.h"
+
+namespace Fortran::tidy {
+namespace modernize {
+
+class ModernizeModule : public FlangTidyModule {
+public:
+  void addCheckFactories(FlangTidyCheckFactories &CheckFactories) override {}
+};
+
+} // namespace modernize
+
+// Register the BugproneTidyModule using this statically initialized variable.
+static FlangTidyModuleRegistry::Add<modernize::ModernizeModule>
+    X("modernize-module", "Adds checks to enforce modern code style.");
+
+// This anchor is used to force the linker to link in the generated object file
+// and thus register the ModernizeModule.
+
+// NOLINTNEXTLINE
+volatile int ModernizeModuleAnchorSource = 0;
+
+} // namespace Fortran::tidy
